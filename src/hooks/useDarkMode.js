b927b53f1130243ac4f0b694ff33b3a2d99dbf09
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-25 16:41:46 -0600

    useLocalStorage and useDarkMode hooks created

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage.js';
+
+const useDarkMode = () => {
+  const [isDark, setIsDark] = useLocalStorage('false')
+
+  useEffect(() => {
+    if(isDark === true) {
+      document.body.classList.add('dark-mode')
+    } else {
+      document.body.classList.remove('dark-mode')
+    }
+  }, [isDark])
+
+  return [isDark, setIsDark]
+}
+
+export default useDarkMode;
